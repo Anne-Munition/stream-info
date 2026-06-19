4f1c9191576b6cd072b8447bba6eccfb6c6cbb7c
DBKynd <db@dbkynd.com>
2026-06-19 13:43:46 -0600

    fix test race condidtion

--- backend/src/directories.ts
+++ backend/src/directories.ts
@@ -5,5 +5,5 @@ export const logDir = path.join(process.cwd(), 'logs');
 export const cacheDir = path.join(process.cwd(), '.cache');
 export const emotesDir = path.join(cacheDir, 'emotes');
 
-if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir);
-if (!fs.existsSync(emotesDir)) fs.mkdirSync(emotesDir);
+fs.mkdirSync(cacheDir, { recursive: true });
+fs.mkdirSync(emotesDir, { recursive: true });
